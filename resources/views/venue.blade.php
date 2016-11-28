a4f8585e73423f71d97c520d818b07b156e77b88
evjohns <ev@evjohns.com>
2016-11-28 20:51:35 +0000

    add another slider

--- resources/views/venue.blade.php
+++ resources/views/venue.blade.php
@@ -171,12 +171,11 @@
 			  </li>
 			</ul>
 		</div>
-		</div>
 
-			<div class="basecamp-vid">
-				<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FGwelanMor%2Fvideos%2F10154438457820219%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-			</div>
+		<div class="basecamp-vid">
+			<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FGwelanMor%2Fvideos%2F10154438457820219%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
 		</div>
+	</div>
 
 		<hr>
 
