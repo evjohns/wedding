fd23164bebd4e9a90d90512f1b4ab1a759f889e3
evjohns <ev@evjohns.com>
2016-11-28 19:16:27 +0000

    text changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,51 +84,78 @@
 $(document).ready(function () {
     function exportTableToCSV($table, filename) {
 
-        var $rows = $table.find('tr:has(td)'),
+        var $headers = $table.find('tr:has(th)')
+                    ,$rows = $table.find('tr:has(td)')
 
-            // Temporary delimiter characters unlikely to be typed by keyboard
-            // This is to avoid accidentally splitting the actual contents
-            tmpColDelim = String.fromCharCode(11), // vertical tab character
-            tmpRowDelim = String.fromCharCode(0), // null character
+                    // Temporary delimiter characters unlikely to be typed by keyboard
+                    // This is to avoid accidentally splitting the actual contents
+                    ,tmpColDelim = String.fromCharCode(11) // vertical tab character
+                    ,tmpRowDelim = String.fromCharCode(0) // null character
 
-            // actual delimiter characters for CSV format
-            colDelim = '","',
-            rowDelim = '"\r\n"',
+                    // actual delimiter characters for CSV format
+                    ,colDelim = '","'
+                    ,rowDelim = '"\r\n"';
 
-            // Grab text from table into CSV formatted string
-            csv = '"' + $rows.map(function (i, row) {
-                var $row = $(row),
-                    $cols = $row.find('td');
+                    // Grab text from table into CSV formatted string
+                    var csv = '"';
+                    csv += formatRows($headers.map(grabRow));
+                    csv += rowDelim;
+                    csv += formatRows($rows.map(grabRow)) + '"';
 
-                return $cols.map(function (j, col) {
+                    // Data URI
+                    var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+                // For IE (tested 10+)
+                if (window.navigator.msSaveOrOpenBlob) {
+                    var blob = new Blob([decodeURIComponent(encodeURI(csv))], {
+                        type: "text/csv;charset=utf-8;"
+                    });
+                    navigator.msSaveBlob(blob, filename);
+                } else {
+                    $(this)
+                        .attr({
+                            'download': filename
+                            ,'href': csvData
+                            //,'target' : '_blank' //if you want it to open in a new window
+                    });
+                }
+
+                //------------------------------------------------------------
+                // Helper Functions 
+                //------------------------------------------------------------
+                // Format the output so it has the appropriate delimiters
+                function formatRows(rows){
+                    return rows.get().join(tmpRowDelim)
+                        .split(tmpRowDelim).join(rowDelim)
+                        .split(tmpColDelim).join(colDelim);
+                }
+                // Grab and format a row from the table
+                function grabRow(i,row){
+                     
+                    var $row = $(row);
+                    //for some reason $cols = $row.find('td') || $row.find('th') won't work...
+                    var $cols = $row.find('td'); 
+                    if(!$cols.length) $cols = $row.find('th');  
+
+                    return $cols.map(grabCol)
+                                .get().join(tmpColDelim);
+                }
+                // Grab and format a column from the table 
+                function grabCol(j,col){
                     var $col = $(col),
-                        text = $col.text();
+                        $text = $col.text();
 
-                    return text.replace('"', '""'); // escape double quotes
+                    return $text.replace('"', '""'); // escape double quotes
 
-                }).get().join(tmpColDelim);
-
-            }).get().join(tmpRowDelim)
-                .split(tmpRowDelim).join(rowDelim)
-                .split(tmpColDelim).join(colDelim) + '"',
-
-            // Data URI
-            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
-
-        $(this)
-            .attr({
-            'download': filename,
-                'href': csvData,
-                'target': '_blank'
-        });
-    }
+                }
+            }
 
 
     // This must be a hyperlink
     $("#export-attending").click(function (event) {
         // var outputFile = 'export'
         alert('hello');
-        var outputFile = 'guests-not-attending';
+        var outputFile = 'guests-attending';
         outputFile = outputFile.replace('.csv','') + '.csv'
          
         // CSV
@@ -141,7 +168,7 @@ $(document).ready(function () {
     $("#export-not-attending").click(function (event) {
         // var outputFile = 'export'
         alert('hello');
-        var outputFile = 'guests-attending';
+        var outputFile = 'guests-not-attending';
         outputFile = outputFile.replace('.csv','') + '.csv'
          
         // CSV
