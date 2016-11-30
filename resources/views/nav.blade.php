2e55e0aa137103abd5d2101aa6bb26394c731e7d
evjohns <ev@evjohns.com>
2016-11-30 18:53:13 +0000

    small styling change to sliders

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -41,11 +41,11 @@ $(".burger-nav").click(function() {
 	$(".nav-links").toggleClass("active");
 
 	if ($(this).hasClass("active")) {
-		$("#page").animate({"margin-right": "250px", "margin-left": "-250px"}, 1000);
-		$("#side-nav").animate({"width": "250px"}, 1000);
+		$("#page").delay(500).animate({"margin-right": "250px", "margin-left": "-250px"}, 200);
+		$("#side-nav").delay(500).animate({"width": "250px"}, 200);
 	} else {
-		$("#page").animate({"margin-right": "0px", "margin-left": "0px"},1000);
-		$("#side-nav").animate({"width": "0"}, 1000);
+		$("#page").delay(500).animate({"margin-right": "0px", "margin-left": "0px"}, 200);
+		$("#side-nav").delay(500).animate({"width": "0"}, 200);
 	}
 });
 </script>
